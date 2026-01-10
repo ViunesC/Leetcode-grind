fc7fbfabb4bcf885f8c6630fd4d0c83aaa20511a
ViunesC <mrfusking233@gmail.com>
2026-01-10 16:57:24 -0500

    lc 763

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import Hard.*;
 
 public class Main {
     public static void main(String[] args) {
-        q435.testSample();
+        q763.testSample();
     }
 }
---
+++ src/Medium/q763.java
@@ -0,0 +1,30 @@
+package Medium;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class q763 {
+    // TODO: rewind
+    public static List<Integer> partitionLabels(String s) {
+        List<Integer> list = new LinkedList<>();
+        int[] edge = new int[26];
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            edge[chars[i] - 'a'] = i;
+        }
+        int idx = 0;
+        int last = -1;
+        for (int i = 0; i < chars.length; i++) {
+            idx = Math.max(idx,edge[chars[i] - 'a']);
+            if (i == idx) {
+                list.add(i - last);
+                last = i;
+            }
+        }
+        return list;
+    }
+
+    public static void testSample() {
+        System.out.println(partitionLabels("eccbbbbdec"));
+    }
+}
