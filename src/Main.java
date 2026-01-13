a426c16f78ec568d618e3b9118ec933cdd4128aa
ViunesC <mrfusking233@gmail.com>
2026-01-13 14:18:45 -0500

    lc 62

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import Hard.*;
 
 public class Main {
     public static void main(String[] args) {
-        q746.testSample();
+        q62.testSample();
     }
 }
---
+++ src/Medium/q62.java
@@ -0,0 +1,23 @@
+package Medium;
+
+public class q62 {
+    public static int uniquePaths(int m, int n) {
+        int[][] grid = new int[m][n];
+        grid[0][0] = 0;
+
+        for (int r=0;r<m;r++) {
+            for (int c=0;c<n;c++) {
+                if (r == 0 || c == 0)
+                    grid[r][c] = 1;
+                else
+                    grid[r][c] = grid[r-1][c] + grid[r][c-1];
+            }
+        }
+
+        return grid[m-1][n-1];
+    }
+
+    public static void testSample() {
+        System.out.println(uniquePaths(3,7));
+    }
+}
