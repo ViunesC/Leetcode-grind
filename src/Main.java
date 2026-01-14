ba6e74c5625ec00cd4c1689e113f24f0f36a7b93
ViunesC <mrfusking233@gmail.com>
2026-01-14 16:20:00 -0500

    lc 96

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import Hard.*;
 
 public class Main {
     public static void main(String[] args) {
-        q343.testSample();
+        q96.testSample();
     }
 }
---
+++ src/Medium/q96.java
@@ -0,0 +1,26 @@
+package Medium;
+
+public class q96 {
+    public static int numTrees(int n) {
+        if (n == 1 || n == 2)
+            return n;
+
+        // if we view each BST as left sub-BST + right sub-BST
+        int[] countSubTree = new int[n+1];
+        countSubTree[0] = 1;
+        countSubTree[1] = 1;
+        countSubTree[2] = 2;
+
+        for (int i=3;i<countSubTree.length;++i) {
+            for (int j=1;j<=i;++j) {
+                countSubTree[i] += countSubTree[j-1] * countSubTree[i-j];
+            }
+        }
+
+        return countSubTree[n];
+    }
+
+    public static void testSample() {
+        System.out.println(numTrees(3));
+    }
+}
