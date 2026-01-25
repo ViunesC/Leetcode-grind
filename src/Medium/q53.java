048b9fbf7b9af8c451b03b5f14a92ee450f1817f
ViunesC <mrfusking233@gmail.com>
2026-01-25 16:25:55 -0500

    lc 53 (dynamic programming)

--- src/Medium/q53.java
+++ src/Medium/q53.java
@@ -2,15 +2,28 @@ package Medium;
 
 public class q53 {
     public static int maxSubArray(int[] nums) {
+        // Greedy solution
         // if sum reaches negative number then discard and pick next as start
         // as negative number only decrease the overall sum
 
-        int result = Integer.MIN_VALUE;
-        int count = 0;
-        for (int i=0;i<nums.length;++i) {
-            count += nums[i];
-            result = Math.max(count, result);
-            if (count < 0) count = 0;
+//        int result = Integer.MIN_VALUE;
+//        int count = 0;
+//        for (int i=0;i<nums.length;++i) {
+//            count += nums[i];
+//            result = Math.max(count, result);
+//            if (count < 0) count = 0;
+//        }
+//
+//        return result;
+        // dp[i]: max sum using numbers from 0 to i
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int result = nums[0];
+
+        for (int i=1;i<nums.length;++i) {
+            dp[i] = Math.max(dp[i-1]+nums[i], nums[i]);
+
+            if (dp[i] > result) result = dp[i];
         }
 
         return result;
