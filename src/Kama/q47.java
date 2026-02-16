bb798be551657acae5ea66366aef41cfe1c960b0
ViunesC <mrfusking233@gmail.com>
2026-02-16 15:41:57 -0500

    kama 47 (min-heap optimized)

--- src/Kama/q47.java
+++ src/Kama/q47.java
@@ -8,24 +8,36 @@ public class q47 {
      * https://kamacoder.com/problempage.php?pid=1047
      */
 
-    private static int[][] adjMatrix;
+    private static class Edge implements Comparable<Edge> {
+        int to;
+        int time;
 
-    public static void main(String[] args) {
+        Edge(int to, int time) {
+            this.to = to;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Edge other) {
+            return Integer.compare(this.time, other.time);  // Min-Heap based on time
+        }
+    }
+
+    private static void trivialDijkstra() {
         Scanner myScanner = new Scanner(System.in);
 
         int N = myScanner.nextInt();
         int M = myScanner.nextInt();
 
-        adjMatrix = new int[N + 1][N + 1];
+        int[] minDist = new int[N + 1];
+        boolean[] visited = new boolean[N + 1];
+        int[][] adjMatrix = new int[N + 1][N + 1];
 
         for (int i = 0; i < M; ++i) {
             adjMatrix[myScanner.nextInt()][myScanner.nextInt()] = myScanner.nextInt();
         }
 
         // System.out.println(Arrays.deepToString(adjMatrix));
-
-        int[] minDist = new int[N + 1];
-        boolean[] visited = new boolean[N + 1];
 
         for (int i = 1; i <= N; ++i)
             minDist[i] = (i == 1) ? 0 : Integer.MAX_VALUE;
@@ -58,5 +70,61 @@ public class q47 {
 
         if (!visited[N]) System.out.println(-1);
         else System.out.println(minDist[N]);
+
+        myScanner.close();
+    }
+
+    private static void minHeapDijkstra() {
+        Scanner myScanner = new Scanner(System.in);
+
+        int N = myScanner.nextInt();
+        int M = myScanner.nextInt();
+
+        List<List<Edge>> adjList = new ArrayList<>();
+        for (int i=0;i<=N;++i)
+            adjList.add(new ArrayList<>());
+
+        for (int i=0;i<M;++i) {
+            adjList.get(myScanner.nextInt()).add(new Edge(myScanner.nextInt(), myScanner.nextInt()));
+        }
+
+        PriorityQueue<Edge> heap = new PriorityQueue<>();
+        heap.add(new Edge(1,0));
+
+        int next;
+        boolean[] visited = new boolean[N+1];
+        int[] minDist = new int[N+1];
+        Arrays.fill(minDist, Integer.MAX_VALUE);
+        minDist[1] = 0;
+
+        for (int i=1;i<=N;++i) {
+            if (heap.isEmpty())
+                break;
+
+            next = heap.poll().to;
+            // System.out.println("next ->" + next);
+            // System.out.println(Arrays.toString(minDist));
+
+            visited[next] = true;
+
+            for (Edge connected : adjList.get(next)) {
+                if (minDist[next] + connected.time < minDist[connected.to])
+                    minDist[connected.to] = minDist[next] + connected.time;
+
+                if (!visited[connected.to])
+                    heap.add(connected);
+            }
+        }
+
+        if (minDist[N] == Integer.MAX_VALUE)
+            System.out.println(-1);
+        else
+            System.out.println(minDist[N]);
+
+        myScanner.close();
+    }
+
+    public static void main(String[] args) {
+        minHeapDijkstra();
     }
 }
