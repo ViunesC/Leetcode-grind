b53d8cab2fe8febfcf72f4cf9d3e9c6132fe9df0
ViunesC <mrfusking233@gmail.com>
2026-03-11 15:31:23 -0400

    lc 416 (python)

---
+++ Python/src/medium/q416.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+        
+        dp = [False for _ in range(total // 2 + 1)] # dp[N], N = total / 2
+        dp[0] = True
+
+        for i in range(len(nums)):
+            for j in range(len(dp)-1, -1, -1):
+                if j >= nums[i]:
+                    dp[j] = dp[j] or dp[j - nums[i]]
+        
+        return dp[len(dp)-1]
