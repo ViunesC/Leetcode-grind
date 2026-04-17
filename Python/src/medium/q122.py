b3c163ada6725a6732b595413a7c932ff5d1a605
ViunesC <mrfusking233@gmail.com>
2026-04-18 06:38:10 +0800

    lc 122 (python)

---
+++ Python/src/medium/q122.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    """
+    Leetcode 122: Best Time to Buy and Sell Stock II
+    """
+
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        
+        return profit
