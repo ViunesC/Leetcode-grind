d17f8000e3c127134c309570869185d1b4ccc009
ViunesC <mrfusking233@gmail.com>
2026-04-24 08:30:45 +0800

    lc 58

---
+++ Python/src/easy/q58.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    Leetcode 58: Length of Last Word
+    """
+    
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip()
+
+        length = 0
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == ' ':
+                break
+            length += 1
+        
+        return length
